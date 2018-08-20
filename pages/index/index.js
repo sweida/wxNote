acaf34dcd244035d29c185a9f21073457d75cbab
sweida <weidaxyy@163.com>
2018-08-21 02:49:27 +0800

    右滑删除

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,32 +4,32 @@ const app = getApp()
 
 Page({
   data: {
-    isfingerPrint : false,    //可否使用指纹识别  默认false
-    isfacial: false,          //可否使用人脸识别  默认false
+    isfingerPrint: false, //可否使用指纹识别  默认false
+    isfacial: false, //可否使用人脸识别  默认false
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../logs/logs',
     })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
         })
       }
     } else {
@@ -39,9 +39,9 @@ Page({
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
           })
-        }
+        },
       })
     }
 
@@ -50,20 +50,34 @@ Page({
     wx.checkIsSupportSoterAuthentication({
       success(res) {
         console.log(res.supportMode, 42332)
-        for (var i in res.supportMode){
-          if (res.supportMode[i] == 'fingerPrint'){
-            console.log("支持指纹识别", res.supportMode[i]);
+        for (var i in res.supportMode) {
+          if (res.supportMode[i] == 'fingerPrint') {
+            console.log('支持指纹识别', res.supportMode[i])
             that.setData({
-              isfingerPrint : true
+              isfingerPrint: true,
             })
-          } else if (res.supportMode[i] == 'facial'){
-            console.log("支持人脸识别", res.supportMode[i]);
+
+            wx.startSoterAuthentication({
+              requestAuthModes: ['fingerPrint'],
+              challenge: '123456',
+              authContent: '通过验证手机指纹解锁',
+              success (res) {
+                console.log('识别成功', res)
+                wx.redirectTo({
+                  url: '../list/list?password=true',
+                })
+              },
+            })
+            
+          } else if (res.supportMode[i] == 'facial') {
+            console.log('支持人脸识别', res.supportMode[i])
           } else {
             console.log('不支持指纹识别和人脸识别')
           }
         }
-      }
+      },
     })
+    // this.FingerPrint()
   },
 
   getUserInfo: function(e) {
@@ -71,25 +85,25 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
     })
   },
 
   //是否可以指纹识别
-  checkIsFingerPrint:function(){
+  checkIsFingerPrint: function() {
     var boole = this.data.isfingerPrint
-    var txt = "不可以使用指纹识别"
+    var txt = '不可以使用指纹识别'
     if (boole) {
-      txt = "可以使用指纹识别"
+      txt = '可以使用指纹识别'
     }
-    show("提示",txt,false);
+    show('提示', txt, false)
   },
   //是否可以人脸识别
-  checkIsFacial: function () {
+  checkIsFacial: function() {
     var boole = this.data.isfacial
-    var txt = "不可以使用人脸识别"
-    if (boole){
-      txt = "可以使用人脸识别"
+    var txt = '不可以使用人脸识别'
+    if (boole) {
+      txt = '可以使用人脸识别'
     }
     // function SUCC() {
     //   console.log("用户点击确定")
@@ -97,87 +111,79 @@ Page({
     // function FAIL() {
     //   console.log("用户点击取消")
     // }
-    show("提示", txt, false);
+    show('提示', txt, false)
   },
 
   //进行指纹识别fingerPrint, 人脸识别facial
-  FingerPrint: function(){
-    if(this.data.isfingerPrint) {
+  FingerPrint: function() {
+    if (this.data.isfingerPrint) {
       wx.startSoterAuthentication({
         requestAuthModes: ['fingerPrint'],
         challenge: '123456',
         authContent: '通过验证手机指纹解锁',
         success(res) {
-          console.log("识别成功",res)
-          // show("提示", "识别成功", false);
-          // wx.showToast({
-          //   title: '识别成功',
-          //   duration: 2000
-          // })
+          console.log('识别成功', res)
           wx.redirectTo({
-            url: '../list/list'
+            url: '../list/list?password=true',
           })
         },
       })
     } else {
-      show("提示", "该手机不支持指纹识别", false);
+      show('提示', '该手机不支持指纹识别', false)
     }
-      // fail(res){
-      //   // console.log("识别失败",res)
-      //   // show("提示", "识别失败", false);
-      // }
+    // fail(res){
+    //   // console.log("识别失败",res)
+    //   // show("提示", "识别失败", false);
+    // }
   },
 
   FingerPrint2() {
     wx.redirectTo({
-      url: '../list/list'
+      url: '../list/list',
     })
   },
   //是否有指纹
-  HaveFingerPrint:function(){
+  HaveFingerPrint: function() {
     wx.checkIsSoterEnrolledInDevice({
       checkAuthMode: 'fingerPrint',
       success(res) {
-        if (res.isEnrolled == 1){
-          show("提示", "有指纹", false);
-        } else if (res.isEnrolled == 0){
-          show("提示", "无指纹", false);
+        if (res.isEnrolled == 1) {
+          show('提示', '有指纹', false)
+        } else if (res.isEnrolled == 0) {
+          show('提示', '无指纹', false)
         }
       },
-      fail(res){
-        show("提示", "异常", fail);
-      }
+      fail(res) {
+        show('提示', '异常', fail)
+      },
     })
-  }
-
+  },
 })
 
-
 /**
-* 显示提示信息
-* tit 提示的标题
-* msg 提示的内容
-* q   是否有取消按钮（布尔值）
-* succ 用户点击确定的回调（非必须）
-* fail 用户点击取消的回调（非必须）
-*
-*/
-function show(tit,msg,q,succ,fail){
+ * 显示提示信息
+ * tit 提示的标题
+ * msg 提示的内容
+ * q   是否有取消按钮（布尔值）
+ * succ 用户点击确定的回调（非必须）
+ * fail 用户点击取消的回调（非必须）
+ *
+ */
+function show(tit, msg, q, succ, fail) {
   wx.showModal({
     title: tit,
     content: msg,
-    showCancel:q,
-    success: function (res) {
+    showCancel: q,
+    success: function(res) {
       if (res.confirm) {
-        if (succ){
-          succ();
+        if (succ) {
+          succ()
         }
       } else if (res.cancel) {
         if (fail) {
-          fail();
+          fail()
         }
       }
-    }
+    },
   })
 }
-
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,71 +1,181 @@
 // pages/list/list.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    noteList: [
+      {
+        id: 1,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 2,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 3,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 4,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 5,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 6,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+      {
+        id: 7,
+        text:
+          '这是记事本内容哈哈哈哈哈哈哈哈哈啊哈啊哈啊哈啊哈哈 啊哈 啊哈啊 啊',
+        creatDate: '2018年08月20日',
+      },
+    ],
+    startX: 0, //开始坐标
+    startY: 0,
   },
 
   detail() {
     wx.navigateTo({
-      url: '../detail/detail'
+      url: '../detail/detail',
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    // if (!options.password) {
+    //   wx.redirectTo({
+    //     url: '../index/index',
+    //   })
+    // }
+    // for (var i = 0; i < 10; i++) {
+    //   this.data.noteList.push({
+    //     content:
+    //       i +
+    //       ' 向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦',
+
+    //     isTouchMove: false, //默认隐藏删除
+    //   })
+    // }
+  },
+
+  //手指触摸动作开始 记录起点X坐标
+  touchstart: function(e) {
+    //开始触摸时 重置所有删除
+    this.data.noteList.forEach(function(v, i) {
+      if (v.isTouchMove)
+        //只操作为true的
+        v.isTouchMove = false
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      noteList: this.data.noteList,
+    })
+  },
+
+  //滑动事件处理
+  touchmove: function(e) {
+    var that = this,
+      index = e.currentTarget.dataset.index, //当前索引
+      startX = that.data.startX, //开始X坐标
+      startY = that.data.startY, //开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX, //滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY, //滑动变化坐标
+      //获取滑动角度
+      angle = that.angle(
+        { X: startX, Y: startY },
+        { X: touchMoveX, Y: touchMoveY },
+      )
+    that.data.noteList.forEach(function(v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return
+      if (i == index) {
+        //右滑
+        if (touchMoveX > startX) {
+          v.isTouchMove = false
+        } else {
+          //左滑
+          v.isTouchMove = true
+        }
+      }
+    })
+    //更新数据
+    that.setData({
+      noteList: that.data.noteList,
+    })
   },
 
   /**
+  * 计算滑动角度
+  * @param {Object} start 起点坐标
+  * @param {Object} end 终点坐标
+  */
+  angle: function(start, end) {
+    var _X = end.X - start.X,
+    _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return (360 * Math.atan(_Y / _X)) / (2 * Math.PI)
+  },
+
+  // 删除事件
+  delItem() {
+    this.data.noteList.splice(e.currentTarget.dataset.index, 1)
+    this.setData({
+      noteList: this.data.noteList
+    })
+  },
+  /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
-  }
+  onShareAppMessage: function() {},
 })
